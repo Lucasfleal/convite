4b60cfa565bbe18233c8cf52fd138c5f0b0a76f9
Lucas Leal <lucasfleal@hotmail.com>
2024-05-24 19:46:11 -0300

    troca para mandar msg

--- script.js
+++ script.js
@@ -5,28 +5,26 @@ document.getElementById('appointmentForm').addEventListener('submit', function(e
     let suggestion = document.getElementById('suggestion').value;
 
     if (date && suggestion) {
-        fetch('https://script.google.com/macros/s/AKfycbwal5_WcY1VCzgNdEp0KHiPFIuFuAj2L2GhoLNinIerINw2b6U_Hjdsjh_mm7hMndcO/exec', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ date, suggestion })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.result === 'success') {
-                document.getElementById('selectedDate').textContent = date;
-                document.getElementById('selectedSuggestion').textContent = suggestion;
-                document.getElementById('appointmentForm').style.display = 'none';
-                document.getElementById('confirmation').style.display = 'block';
-            } else {
-                alert('Erro ao salvar os dados.');
-            }
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-            alert('Erro ao enviar os dados.');
-        });
+        let redirectUrl = '';
+
+        // Verifica o dia selecionado e define o URL de redirecionamento correspondente
+        switch (date) {
+            case '30/05':
+                redirectUrl = 'https://api.whatsapp.com/send?phone=+5519971010084&text=Ol%C3%A1%2C+ap%C3%B3s+uma+grande+analise%2C+concordo+em+lhe+conceder+a+oportunidade+de+estar+em+minha+presen%C3%A7a+no+dia+30%2F05+e+sugiro+' + suggestion;
+                break;
+            case '31/05':
+                redirectUrl = 'https://api.whatsapp.com/send?phone=+5519971010084&text=Ol%C3%A1%2C+ap%C3%B3s+uma+grande+analise%2C+concordo+em+lhe+conceder+a+oportunidade+de+estar+em+minha+presen%C3%A7a+no+dia+31%2F05+e+sugiro+' + suggestion;
+                break;
+            case '01/06':
+                redirectUrl = 'https://api.whatsapp.com/send?phone=+5519971010084&text=Ol%C3%A1%2C+ap%C3%B3s+uma+grande+analise%2C+concordo+em+lhe+conceder+a+oportunidade+de+estar+em+minha+presen%C3%A7a+no+dia+01%2F06+e+sugiro+' + suggestion;
+                break;
+            case '02/06':
+                redirectUrl = 'https://api.whatsapp.com/send?phone=+5519971010084&text=Ol%C3%A1%2C+ap%C3%B3s+uma+grande+analise%2C+concordo+em+lhe+conceder+a+oportunidade+de+estar+em+minha+presen%C3%A7a+no+dia+02%2F06+e+sugiro+' + suggestion;
+                break;
+        }
+
+        // Redireciona o usuário para o URL correspondente
+        window.location.href = redirectUrl;
     } else {
         alert('Por favor, preencha todos os campos.');
     }
